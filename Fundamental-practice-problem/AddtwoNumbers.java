bfda16e95b80edbdbfb20c1a1ad2ff515d1e461b
Unnati13 <unnatiagarwal1313@gmail.com>
2025-08-14 10:38:03 +0530

    [Unnati]- Rafactored - Renaming functional problems

--- Fundamental-practice-problem/AddtwoNumbers.java
+++ Fundamental-practice-problem/AddtwoNumbers.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Q2 {
+public class AddtwoNumbers{
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter first no.: ");
