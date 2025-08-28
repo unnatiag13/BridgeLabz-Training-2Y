badb45b8bd393e9307f4298321124442dd45415b
Unnati13 <unnatiagarwal1313@gmail.com>
2025-08-28 10:41:17 +0530

    [Unnati] Added Question-Can vote

---
+++ Core-java/Arrays/Level-1/canVote.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class canVote {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] age = new int[10];
+        
+        for(int i=0;i<age.length;i++) age[i] = sc.nextInt();
+
+        for(int i=0;i<age.length;i++){
+            if(age[i]<10) System.out.println("Invalid age");
+            else{
+                if(age[i]>=18){
+                    System.out.println("The student with the age "+ age[i]+" can vote.");
+                }
+                else{
+                    System.out.println("The student with age "+age[i]+" can not vote");
+                }
+            }
+        }
+        
+        
+    }
+}
