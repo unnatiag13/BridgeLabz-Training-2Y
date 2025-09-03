e643bf7cd1150490063357cb5b847baf85921002
Unnati13 <unnatiagarwal1313@gmail.com>
2025-09-03 12:03:23 +0530

    [Unnati] Added Simple interest Question

---
+++ Core-java/Methods/Level-1/SimpleInterestCalculator.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class SimpleInterestCalculator {
+    public static double simpleInterest(double principal , double rate , double time){
+        double simple_interest = (principal * rate * time) / 100;
+        return simple_interest;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Principal: ");
+        double p = sc.nextDouble();
+        System.out.print("Enter Rate: ");
+        double r = sc.nextDouble();
+        System.out.print("Enter Time: ");
+        double t = sc.nextDouble();
+
+        double si = simpleInterest(p, r, t);
+        System.out.println("The Simple Interest is " + si +
+                " for Principal " + p + ", Rate of Interest " + r + " and Time " + t);
+    }
+}
