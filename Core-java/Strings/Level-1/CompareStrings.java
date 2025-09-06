31eb3f3cb3c1b13c1ddea5a45374e619fedfbda1
Unnati13 <unnatiagarwal1313@gmail.com>
2025-09-06 20:18:08 +0530

    [Unnati] Added first strings question

--- Core-java/Strings/Level-1/CompareStrings.java
+++ Core-java/Strings/Level-1/CompareStrings.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 public class CompareStrings {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s1 = sc.next();
-        String s2 = sc.next();
-        boolean areEqual=true;
-        if(s1.length()!=s2.length()) System.out.println("Not equal");
-        else{
-            for(int i=0;i<s1.length();i++){
+        public static boolean areEqual(String s1,String s2){
+            boolean areEqual=true;
+             for(int i=0;i<s1.length();i++){
                 if(s1.charAt(i)!=s2.charAt(i)){
                     areEqual=false;
                 }
             }
-            System.out.println("Are both strings equal: "+areEqual+"\n using built-in function: "+s1.equals(s2));
+            return areEqual;
+        }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        if(s1.length()!=s2.length()) System.out.println("Not equal");
+        else{
+            System.out.println("Are both strings equal: "+areEqual(s1,s2)+"\n using built-in function: "+s1.equals(s2));
         }
     }
 }
