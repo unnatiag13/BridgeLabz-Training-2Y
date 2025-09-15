5cd4d0b212d6ae2af820e0aa497d183a1945eb31
Unnati13 <unnatiagarwal1313@gmail.com>
2025-09-15 12:26:04 +0530

    Commit

--- Core-java/Extras/StringFucntions/CountSubstrings.java
+++ Core-java/Extras/StringFucntions/CountSubstrings.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class CountSubstring{
+public class CountSubstrings{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a String: ");
