f17ef19ecc02b26f3fe1d6a5d1fdc35a5e8bfeeb
Unnati13 <unnatiagarwal1313@gmail.com>
2025-10-15 23:27:48 +0530

    [Unnati] Added Unit converter question

---
+++ interfaces/UnitConverter.java
@@ -0,0 +1,20 @@
+public interface UnitConverter {
+    static double kmToMiles(double km) {
+        return km * 0.621371;
+    }
+
+    static double kgToLbs(double kg) {
+        return kg * 2.20462;
+    }
+}
+
+class TestConversion {
+    public static void main(String[] args) {
+        double km = 10;
+        double kg = 5;
+
+        System.out.println(km + " km = " + UnitConverter.kmToMiles(km) + " miles");
+        System.out.println(kg + " kg = " + UnitConverter.kgToLbs(kg) + " lbs");
+    }
+}
+
